17278523eb7e3499d0fc7a826ea66178be90c4c8
Richard Ma <richard.ma.19850509@gmail.com>
2014-08-29 20:21:32 +0800

    fix PageSpider can\'t find attribute rules bug

--- archive/spiders/page_spider.py
+++ archive/spiders/page_spider.py
@@ -4,35 +4,27 @@ from scrapy.selector import HtmlXPathSelector
 
 from archive.items import PageItem
 
-from ConfigParser import ConfigParser
-
 class PageSpider(CrawlSpider):
-    config = ConfigParser()
-    name = 'page'
+    name = "page"
     allowed_domains = ["web.archive.org"]
-
+    #allowed_domains = ["mobilezonegt.com"]
     start_urls = [
             "http://web.archive.org/web/20120114003207/http://mobilezonegt.com/",
+            #"http://mobilezonegt.com/"
             ]
 
     rules = (
+            #Rule(SgmlLinkExtractor(allow=(r'.*')),
+                #callback='parse_item',
+                #follow=True),
             Rule(SgmlLinkExtractor(allow=(r'.*/http://mobilezonegt\.com/.*')),
                 callback='parse_item',
                 follow=True),
             )
 
-    def __init__(self):
-        self.config.read('./configrations.ini')
-        #self.allowed_domains = ['web.archive.org']
-        #self.start_urls = ['http://web.archive.org/web/20120114003207/http://mobilezonegt.com/']
-        #self.rules = [
-            #Rule(SgmlLinkExtractor(allow=(r'.*/http://mobilezonegt\.com/.*')), callback='parse_item', follow=True),
-            #]
-
     def parse_item(self, response):
+        #hxs = HtmlXPathSelector(response)
         item = PageItem()
-
         item['url'] = response.url
         item['content'] = response.body
-
         return item
